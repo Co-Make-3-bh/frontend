4eb1cb2367cc331e9f7e066fbc7f7a66e10b723a
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-07-27 10:35:35 -0700

    fetching from backend

--- co-make/src/App.js
+++ co-make/src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>App</h1>
+        <HomePage />
       </header>
+      <Switch>
+        <Route path="/home" component={HomePage} />
+      </Switch>
     </div>
   );
 }
--- co-make/src/components/HomePage.js
+++ co-make/src/components/HomePage.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { fetchConcerns } from "../store/actions";
+import { useSelector, useDispatch } from "react-redux";
 
-const HomePage = () => {};
+const HomePage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchConcerns());
+  }, []);
+
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
+};
 
 export default HomePage;
