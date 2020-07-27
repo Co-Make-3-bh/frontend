74c46331847298d8b4fa219ddbfe3bc9ef42ce3d
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-07-27 14:13:14 -0700

    login hitting 400

--- co-make/src/components/HomePage.js
+++ co-make/src/components/HomePage.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 const HomePage = () => {
   const dispatch = useDispatch();
+  const { concerns } = useSelector((state) => state);
   useEffect(() => {
     dispatch(fetchConcerns());
   }, []);
@@ -11,6 +12,16 @@ const HomePage = () => {
   return (
     <div>
       <h1>HomePage</h1>
+      {concerns.map((concern) => {
+        console.log(concern);
+        return (
+          <div key={concern.id}>
+            <h2>{concern.title}</h2>
+            <p>{concern.description}</p>
+            <p>{concern.zip}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- co-make/src/components/PostIssue/issueForm.jsx
+++ co-make/src/components/PostIssue/issueForm.jsx
@@ -1,122 +1,117 @@
-import React, { useState } from 'react'
-import axios from 'axios';
-import { IssueSchema } from './issueSchema';
+import React, { useState } from "react";
+import axios from "axios";
+import { IssueSchema } from "./issueSchema";
 
+const IssueForm = (props) => {
+  const initialValues = {
+    title: "",
+    description: "",
+    createdBy: "",
+    zipCode: "",
+  };
 
-const IssueForm = props => {
+  const [formValues, setFormValues] = useState(initialValues);
 
-    const initialValues = {
-        title: "",
-        description: "",
-        createdBy:"",
-        zipCode:"",
-    }
+  const [errors, setErrors] = useState([]);
 
-    const [formValues, setFormValues] = useState(initialValues);
+  const handleChange = (e) => {
+    console.dir(e.target);
 
-    const [errors, setErrors] = useState([]);
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    const handleChange = e => {
+    IssueSchema.validate(formValues, { abortEarly: false })
+      .then((_) => {
+        axios
+          .post("https://comake-api.herokuapp.com/api/concerns", formValues)
+          .then((res) => {
+            if (errors.length > 0) {
+              setErrors([]);
+            }
 
-        console.dir(e.target);
+            console.log(res);
 
-        setFormValues({
-            ...formValues, [e.target.name]: e.target.value
-        });
+            props.setIssues([...props.issues, res.data]);
 
-       
-    }
-
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        IssueSchema.validate(formValues, {abortEarly: false})
-        .then(_=> {
-            axios.post('https://comake-api.herokuapp.com/api/concerns',formValues)
-            .then(res => {
-                if(errors.length > 0){
-                    setErrors([]);
-                }
-
-                console.log(res);
-
-                props.setIssues([...props.issues, res.data])
-
-                setFormValues(initialValues);
-
-            })
-            .catch(err => {
-
-                console.dir(err);
-            });
-        })
-        .catch(err => {
+            setFormValues(initialValues);
+          })
+          .catch((err) => {
             console.dir(err);
+          });
+      })
+      .catch((err) => {
+        console.dir(err);
 
-            setErrors([...err.inner]);
-        })
-    };
+        setErrors([...err.inner]);
+      });
+  };
 
-    return(
-        <div>
-        <h2>Post an Issue</h2>
-        <form>
-           <label>Title:&nbsp;
-                <input
-                    name="title"
-                    type="text"
-                    data-cy="input-title"
-                    onChange={handleChange}
-                    value={formValues.title}
-                    />
-            </label>
+  return (
+    <div>
+      <h2>Post an Issue</h2>
+      <form>
+        <label>
+          Title:&nbsp;
+          <input
+            name="title"
+            type="text"
+            data-cy="input-title"
+            onChange={handleChange}
+            value={formValues.title}
+          />
+        </label>
 
-            <label>Description:&nbsp;
-                <input
-                    name="description"
-                    type="text"
-                    data-cy="input-description"
-                    onChange={handleChange}
-                    value={formValues.description}
-                    />
-            </label>
+        <label>
+          Description:&nbsp;
+          <input
+            name="description"
+            type="text"
+            data-cy="input-description"
+            onChange={handleChange}
+            value={formValues.description}
+          />
+        </label>
 
-            <label>Created By:&nbsp;
-                <input
-                    name="createdBy"
-                    type="text"
-                    data-cy="input-createdby"
-                    onChange={handleChange}
-                    value={formValues.createdBy}
-                    />
-            </label>
-            
-            <label>Zip Code:&nbsp;
-                <input
-                    name="zipCode"
-                    type="text"
-                    data-cy="input-zipcode"
-                    onChange={handleChange}
-                    value={formValues.zipCode}
-                    />
-            </label>
-            
-            
-            <button data-cy="submit-button" onClick={handleSubmit}>Post New Issue</button>
-            
-            <div data-cy="error-output">
-                {errors.map( err => (  
-                    <p style={{color: "red"}}>{err.message}</p>
-                ))}
-            </div>
-        </form>
+        <label>
+          Created By:&nbsp;
+          <input
+            name="createdBy"
+            type="text"
+            data-cy="input-createdby"
+            onChange={handleChange}
+            value={formValues.createdBy}
+          />
+        </label>
+
+        <label>
+          Zip Code:&nbsp;
+          <input
+            name="zipCode"
+            type="text"
+            data-cy="input-zipcode"
+            onChange={handleChange}
+            value={formValues.zipCode}
+          />
+        </label>
+
+        <button data-cy="submit-button" onClick={handleSubmit}>
+          Post New Issue
+        </button>
+
+        <div data-cy="error-output">
+          {errors.map((err) => (
+            <p style={{ color: "red" }}>{err.message}</p>
+          ))}
         </div>
-    )
+      </form>
+    </div>
+  );
+};
 
-}
-
-
-export default IssueForm 
+export default IssueForm;
