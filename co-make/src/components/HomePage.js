3d72026235718bbb5b7886c7c0cebf191bb8e1f2
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-07-29 10:01:40 -0700

    upvotes implemented and rendering to DOM

--- co-make/src/components/HomePage.js
+++ co-make/src/components/HomePage.js
@@ -1,11 +1,30 @@
 import React, { useEffect } from "react";
-import { fetchConcerns } from "../store/actions";
 import { useSelector, useDispatch } from "react-redux";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+
+import { fetchConcerns, addUpvote } from "../store/actions";
+
+const UpVotes = styled.div`
+  width: 3%;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  margin: 0 auto;
+
+  .upvote {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+`;
 
 const HomePage = () => {
   const dispatch = useDispatch();
   const { concerns } = useSelector((state) => state.concernsReducer);
   const { user } = useSelector((state) => state.userReducer);
+  console.log(concerns);
   useEffect(() => {
     dispatch(fetchConcerns());
   }, []);
@@ -20,6 +39,16 @@ const HomePage = () => {
             <h2>{concern.title}</h2>
             <p>{concern.description}</p>
             <p>{concern.zip}</p>
+            <UpVotes>
+              <p>{concern.upvotes}</p>
+              <FontAwesomeIcon
+                className="upvote"
+                onClick={() => {
+                  dispatch(addUpvote(concern.id));
+                }}
+                icon={faAngleUp}
+              />
+            </UpVotes>
           </div>
         );
       })}
--- co-make/src/store/reducers/concernsReducer.js
+++ co-make/src/store/reducers/concernsReducer.js
@@ -5,13 +5,16 @@ import {
   FETCH_CONCERN_START,
   FETCH_CONCERN_FAILURE,
   FETCH_CONCERN_SUCCESS,
+  ADD_UPVOTE_START,
+  ADD_UPVOTE_SUCCESS,
+  ADD_UPVOTE_FAILURE,
 } from "../actions";
 
 export const initialState = {
   concerns: [],
   userConcerns: [],
   isFetching: false,
-  isEditing: false,
+  isUpdating: false,
   isDeleting: false,
   error: "",
   message: "",
@@ -33,6 +36,21 @@ const concernsReducer = (state = initialState, action) => {
       return { ...state, isFetching: false, concerns: action.payload };
     case FETCH_CONCERNS_FAILURE:
       return { ...state, isFetching: false, error: action.payload };
+    case ADD_UPVOTE_START:
+      return { ...state, isUpdating: true, error: "" };
+    case ADD_UPVOTE_SUCCESS:
+      return {
+        ...state,
+        isUpdating: false,
+        concerns: state.concerns.map((concern) => {
+          if (concern.id === action.payload.id) {
+            return action.payload;
+          }
+          return concern;
+        }),
+      };
+    case ADD_UPVOTE_FAILURE:
+      return { ...state, isUpdating: false, error: action.payload };
     default:
       return state;
   }
