9d9b3799087060e511a0c3fe39f6e4925e83a350
Bender0239 <hayden.bender@protonmail.com>
2020-07-29 10:12:20 -0700

    edit and details done

--- co-make/src/components/PostIssue/issue.jsx
+++ co-make/src/components/PostIssue/issue.jsx
@@ -2,6 +2,79 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { deleteConcern, editConcern } from "../../store/actions";
+import styled from 'styled-components'
+
+
+const FormContainer = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  background-color: #e5ebed;
+  border-radius: 20px;
+  font-family: "Quicksand", sans-serif;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  h2{
+    text-align: left;
+    font-size: 2rem;
+  }
+  input{
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 1px solid lightgrey;
+    border-radius: 7px;
+    height: 50px;
+    font-size: 1.1rem;
+  }
+  button{
+    margin-top: 20px;
+    height: 50px;
+    border-radius: 7px;
+    border: none;
+    background-color: #2B85A2;
+    color: white;
+    font-size: 1.3rem;
+  }
+`
+const StyledDetailsCon = styled.div`
+  width: 40%;
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #e5ebed;
+  border-radius: 20px;
+  font-family: "Quicksand", sans-serif;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  button{
+    margin-top: 20px;
+    height: 50px;
+    border-radius: 7px;
+    border: none;
+    background-color: #2B85A2;
+    color: white;
+    font-size: 1.3rem;
+    width: 100px;
+    margin: 10px;
+  }
+  .buttonDiv{
+    margin-right: 30%;
+  }
+  p{
+    text-align: left;
+    margin-left: 15%;
+    margin-top: 40px;
+    font-size: 1.5rem;
+  }
+  h2{
+    text-align: left;
+    margin-left: 15%;
+    margin-top: 30px;
+    font-size: 1.5rem;
+  }
+`
 
 const Issue = (props) => {
   const [isEditing, setIsEditing] = useState(false);
@@ -53,18 +126,25 @@ const Issue = (props) => {
   //   </div>
   // );
   return !isEditing ? (
+    <StyledDetailsCon>
     <div>
+      <h2>Post Details:</h2>
       <p>Title: {state.title}</p>
       <p>Description: {state.description}</p>
-      <p>Created By: {state.username}</p>
+      {/* <p>Created By: {state.username}</p> */}
       <p>Zip Code: {state.zip}</p>
+      <div className='buttonDiv'>
       <button onClick={handleDelete}>Delete</button>
       <button onClick={isEditing ? handleSubmit : handleEdit}>
         {isEditing ? "Submit" : "Edit"}
       </button>
+      </div>
     </div>
+    </StyledDetailsCon>
   ) : (
-    <form onSubmit={handleSubmit}>
+    <FormContainer>
+      <StyledForm onSubmit={handleSubmit}>
+      <h2>Edit Post:</h2>
       <input
         type="text"
         name="title"
@@ -84,7 +164,8 @@ const Issue = (props) => {
         onChange={handleChanges}
       ></input>
       <button>Submit</button>
-    </form>
+      </StyledForm>
+    </FormContainer>
   );
 };
 
